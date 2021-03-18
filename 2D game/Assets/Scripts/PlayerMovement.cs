4a1341e71c04c6bb0124bbaea8efb3003e55028d
beaumonton <66109136+beaumonton@users.noreply.github.com>
2021-03-17 20:56:49 -0700

    Revert "BeauMechanicMerge"
    This reverts commit d6bd36c26eb28076b127dcba19cc323033eeeaca, reversing
    changes made to 61d4e60f784e5f4b94e25c0f068aaa17502c44d4.

--- 2D game/Assets/Scripts/PlayerMovement.cs
+++ 2D game/Assets/Scripts/PlayerMovement.cs
@@ -6,32 +6,14 @@ public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed;
     public Rigidbody2D rb;
+
     public Animator anim;
-    public BoxCollider2D playerCollider;
-
-    public float crouchHeightPercent = 0.5f;
-    private Vector2 standColliderSize;
-    private Vector2 standColliderOffset;
-    private Vector2 crouchColliderSize;
-    private Vector2 crouchColliderOffset;
-
 
     public float jumpForce = 30f;
     public Transform feet;
     public LayerMask groundLayers;
 
-    [HideInInspector] public bool isFacingRight = true;
-
     float movementX;
-
-    private void Awake()
-    {
-        standColliderSize = playerCollider.size;
-        standColliderOffset = playerCollider.offset;
-
-        crouchColliderSize = new Vector2(standColliderSize.x, standColliderSize.y * crouchHeightPercent);
-        crouchColliderOffset = new Vector2(standColliderOffset.x, standColliderOffset.y * crouchHeightPercent);
-    }
 
     public void Update()
     {
@@ -41,17 +23,6 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump") && isGrounded())
         {
             Jump();
-        }
-
-        if(Input.GetKey(KeyCode.S) && isGrounded())
-        {
-            anim.SetBool("isCrouching", true);
-            Crouch();
-        }
-        else
-        {
-            anim.SetBool("isCrouching", false);
-            StandUp();
         }
 
         if(Mathf.Abs(movementX) > 0.05f)
@@ -66,12 +37,10 @@ public class PlayerMovement : MonoBehaviour
         if (movementX > 0f) //If Player is moving right
         {
             transform.localScale = new Vector3(3f, 3f, 3f); //Must equal Player Object's scale values
-            isFacingRight = true;
         }
         else if (movementX < 0f)//If Player is moving left
         {
             transform.localScale = new Vector3(-3f, 3f, 3f); //Must equal Player Object's scale values with -1 * x value
-            isFacingRight = false;
         }
 
         anim.SetBool("isGrounded", isGrounded());
@@ -89,18 +58,6 @@ public class PlayerMovement : MonoBehaviour
         Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
 
         rb.velocity = movement;
-    }
-
-    void Crouch()
-    {
-        playerCollider.size = crouchColliderSize;
-        playerCollider.offset = crouchColliderOffset;
-    }
-
-    void StandUp()
-    {
-        playerCollider.size = standColliderSize;
-        playerCollider.offset = standColliderOffset;
     }
 
     public bool isGrounded() //Returns true if player is on the ground
